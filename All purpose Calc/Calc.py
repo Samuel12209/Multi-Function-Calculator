6e5cabe6fdbb13756fae3004046b09374349619f
Samuel12209 <samuelbenitez12290@gmail.com>
2024-03-07 15:56:28 -0500

    A few floats have been turned into int and now and input is now alot eaiser(you dont need to put exact capatilizing)

--- All purpose Calc/Calc.py
+++ All purpose Calc/Calc.py
@@ -68,22 +68,22 @@ if mathing == 6:
 if mathing == 7:
     print("Pythagorean Therum")
     print("-"*10)
-    typep = input("What are you solving for?: ")
-    if typep =="C":
+    typep = input("What are you solving for?: ").lower()
+    if typep =="c":
         a = int(input("A: "))
         b = int(input("B: "))
         c = math.hypot(a,b)
         print("-"*10)
         print(c)
         print("-"*10)
-    if typep =="A":
+    if typep =="a":
         c = int(input("C: "))
         b = int(input("B: "))
         a = math.sqrt(c ** 2 - b ** 2)
         print("-"*10)
         print(a)
         print("-"*10)
-    if typep =="B":
+    if typep =="b":
         a = int(input("A: "))
         c = int(input("C: "))
         b = math.sqrt(c ** 2 - a ** 2)
@@ -93,38 +93,38 @@ if mathing == 7:
 
 if mathing == 8:
     print("Pi")
-    math_type = input("Radius, Circ, Area: ")
+    math_type = input("Radius, Circ, Area: ").lower()
     print(math_type)
-    if math_type == "Radius":
+    if math_type == "radius":
       print("-"*10)
       area = int(input("Area: "))
       radius = math.sqrt(area / math.pi)
       print(radius)
       print("-"*10)
-    if math_type =="Circ":
+    if math_type =="circ":
       print("-"*10)
-      radius = float(input("Radius: "))
+      radius = int(input("Radius: "))
       circumference = 2 * math.pi * radius
       print(circumference)
       print("-"*10)
 
-    if math_type =="Area":
+    if math_type =="area":
       print("-"*10)
-      radius = float(input("Radius: "))
+      radius = int(input("Radius: "))
       area = math.pi * radius ** 2
       print(area)
       print("-"*10)
 if mathing == 9:
   print("sqaure")
   print("-"*10)
-  mathtype = input("Area, Perimiter: ")
-  if mathtype == "Area":
+  mathtype = input("Area, Perimiter: ").lower()
+  if mathtype == "area":
     print("Area")
     n1 = int(input("Side: "))
     n2 = int(input("Side: "))
     n3 = n1 * n2
     print(n3)
-  if mathtype =="Perimiter":
+  if mathtype =="perimiter":
     print("Perimiter")
     n1 = int(input("Side1: "))
     n2 = int(input("Side2: "))
@@ -132,4 +132,4 @@ if mathing == 9:
     print(n3)
 
 print("Thanks for using!")
-yesno = input("Finsished?(Enter To Escape)")
+yesno = input("Finsished?(Enter To Escape)").lower
